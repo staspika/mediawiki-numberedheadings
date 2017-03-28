5c2de39047bdde4357bbfa5811438b702af333d2
Stanislav Pika <staspika@gmail.com>
2017-03-28 16:27:55 +0200

    Get rid of legacy code that is no longer necessary.

--- NumberedHeadings.class.php
+++ NumberedHeadings.class.php
@@ -5,32 +5,15 @@ if (!defined('MEDIAWIKI')) {
 }
 
 class NumberedHeadings { 
-    static public function onMagicWordMagicWords(&$magicWords)
-    {
-        $magicWords[] = 'MAG_NUMBEREDHEADINGS';
-        return true;
-    }
-     
-    static public function onMagicWordwgVariableIDs(&$wgVariableIDs)
-    {
-        $wgVariableIDs[] = 'MAG_NUMBEREDHEADINGS';
-        return true;
-    }
-     
-    #function MagicNumberedHeadingsParserBeforeInternalParse($parser, $text, $stripState)
-    static public function onInternalParseBeforeLinks(&$parser, &$text, &$stripState)
-    {
-        if (MagicWord::get( 'MAG_NUMBEREDHEADINGS' )->matchAndRemove( $text ) )
-        {
-                $parser->mOptions->setNumberHeadings(true);
-        }
-        return true;
-    }
+   #function MagicNumberedHeadingsParserBeforeInternalParse($parser, $text, $stripState)
     static public function onParserBeforeInternalParse(&$parser, &$text, &$stripState)
     {
-        if (MagicWord::get( 'MAG_NONUMBEREDHEADINGS' )->matchAndRemove( $text ) ) {
+        if (MagicWord::get( 'MAG_NUMBEREDHEADINGS' )->matchAndRemove( $text ) ) {
+            $parser->mOptions->setNumberHeadings(true);
+        }
+        elseif (MagicWord::get( 'MAG_NONUMBEREDHEADINGS' )->matchAndRemove( $text ) ) {
             $parser->mOptions->setNumberHeadings(FALSE);
         }
-        return true;
+    return true;
     }
 }
